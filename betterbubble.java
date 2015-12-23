bb15cf6c91c29c407cb5796a5dd156f60d883a95
Karin Touchie <ktouchie@gmail.com>
2015-12-23 11:14:12 +0100

    Duplicate bubble sort, replace for loop with while loop, add swap variable

---
+++ betterbubble.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class betterbubble {
+
+	public static void main(String[] args) {
+        
+        int[] tab = {5,9,4,1,2,7,3,8,6,0};
+        
+        boolean swap = false;
+        
+        do {
+            swap = false;
+            
+            int i = 0;
+            int j = tab.length-1;
+            int currentSwap;
+            
+            while (i<j) {
+                
+                if (tab[i]>tab[i+1]) {
+                    int x = tab[i];
+                    tab[i] = tab[i+1];
+                    tab[i+1] = x;
+                    swap = true;
+                    currentSwap = i+1;
+                    i++;                    
+                }
+                j = currentSwap;
+            }
+        } while (swap == true);
+        System.out.println(Arrays.toString(tab));
+    }
+}
