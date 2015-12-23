dd73f69a0582794528d4ef48e4ca31d243714246
Karin Touchie <ktouchie@gmail.com>
2015-12-23 11:33:26 +0100

    Compile

--- betterbubble.java
+++ betterbubble.java
@@ -6,26 +6,28 @@ class betterbubble {
         int[] tab = {5,9,4,1,2,7,3,8,6,0};
         
         boolean swap = false;
+        int j = tab.length-1;
+        int currentSwap = -1;
         
         do {
             swap = false;
-            
+        
             int i = 0;
-            int j = tab.length-1;
-            int currentSwap;
-            
+           
             while (i<j) {
                 
                 if (tab[i]>tab[i+1]) {
                     int x = tab[i];
                     tab[i] = tab[i+1];
                     tab[i+1] = x;
-                    swap = true;
-                    currentSwap = i+1;
-                    i++;                    
+                    swap = true;                   
                 }
-                j = currentSwap;
+                currentSwap = i+1;
+                i++;
             }
+            
+            j = currentSwap;
+            
         } while (swap == true);
         System.out.println(Arrays.toString(tab));
     }
